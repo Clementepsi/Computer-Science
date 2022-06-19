0d41cd1333d1544bdfed1de9c44646e3ba95a448
Clemente Flores <fmlclemente@hotmail.com>
2022-06-19 09:29:52 -0700

    feat: Base de CSS HTML y JS

---
+++ project 1/js/index1.js
@@ -0,0 +1,10 @@
+// const ulElement = document.getElementById('list-languages');
+// getElementById
+// getElementByClassName
+// getElementByTagName
+const renderElementList = () => {
+    const liElement = document.createElement('li');
+    const iElement = document.createElement('i');
+    const divElement = document.createElement('div');
+    const buttonElement = document.createElement('button');
+}
